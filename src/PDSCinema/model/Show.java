642fd0bd6ca26caa5450571a807f5b26f5ba9273
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-22 17:57:29 -0300

    Update Show.java

--- src/PDSCinema/model/Show.java
+++ src/PDSCinema/model/Show.java
@@ -3,7 +3,7 @@ package PDSCinema.model;
 public class Show {
   
     private static String atracaoMusical;
-    private static String horario;
+    private static String duracao;
     private static String data;
     private static List<Integer> avaliacaoesShow = new ArrayList<>();
     private static int qntAvaliacoesShow;
