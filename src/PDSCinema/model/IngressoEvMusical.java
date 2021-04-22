528a11cbcb81921b01d2c991ca709ee86f259ba0
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-22 18:08:47 -0300

    Update IngressoEvMusical.java

--- src/PDSCinema/model/IngressoEvMusical.java
+++ src/PDSCinema/model/IngressoEvMusical.java
@@ -1,4 +1,13 @@
 package PDSCinema.model;
 
 public class IngressoEvMusical extends Ingresso{
+
+    private Show show;
+
+    public Show getShow() {
+        return show;
+    }
+    public void setShow(Show show) {
+        this.show = show;
+    }
 }
