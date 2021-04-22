3f1d48063cdbdaa4b700f28e588c8a1371d789f7
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-22 17:48:00 -0300

    Update EvMusicalRepository.java

--- src/PDSCinema/repository/EvMusicalRepository.java
+++ src/PDSCinema/repository/EvMusicalRepository.java
@@ -1,5 +1,24 @@
 package PDSCinema.repository;
 
+import PDSCinema.model.*;
+import PDSCinema.service.ClienteService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class EvMusicalRepository extends EventoRepository{
 
+    private static List<Show> ShowEmCartaz = new ArrayList<>();
+
+   
+    public static List<Show> getShowEmCartaz() {
+        return filmesEmCartaz;
+    }
+
+    public static void setShowEmCartaz(List<Show> ShowEmCartaz) {
+        EvMusicalRepository.ShowEmCartaz = ShowEmCartaz;
+    }
+
 }
+
