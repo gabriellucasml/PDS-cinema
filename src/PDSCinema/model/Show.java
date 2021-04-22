af12a7b546e80a08ed96e42fa2f55697cfffc239
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-22 18:09:25 -0300

    Update Show.java

--- src/PDSCinema/model/Show.java
+++ src/PDSCinema/model/Show.java
@@ -4,10 +4,6 @@ public class Show extends Evento{
   
     private static String atracaoMusical;
     private static String data;
-    private static List<Integer> avaliacaoesShow = new ArrayList<>();
-    private static int qntAvaliacoesShow;
-    private static List<Integer> avaliacoesLocal;= new ArrayList<>();
-    private static int qntAvaliacoesLocal;
   
  
 }
