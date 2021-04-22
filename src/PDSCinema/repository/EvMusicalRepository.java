3c399917583bf2504f2ffb85f8f3043e06161e36
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-22 18:10:12 -0300

    Update EvMusicalRepository.java

--- src/PDSCinema/repository/EvMusicalRepository.java
+++ src/PDSCinema/repository/EvMusicalRepository.java
@@ -10,6 +10,10 @@ import java.util.List;
 public class EvMusicalRepository extends EventoRepository{
 
     private static List<Show> ShowEmCartaz = new ArrayList<>();
+    private static List<Integer> avaliacaoesShow = new ArrayList<>();
+    private static int qntAvaliacoesShow;
+    private static List<Integer> avaliacoesLocal;= new ArrayList<>();
+    private static int qntAvaliacoesLocal;
 
    
     public static List<Show> getShowEmCartaz() {
