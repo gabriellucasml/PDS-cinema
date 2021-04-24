505a953db7567a27600969d8f4f8e08b20c278fe
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-24 15:15:12 -0300

    Update ClienteEvMusical.java

--- src/PDSCinema/service/ClienteStrategy/ClienteEvMusical.java
+++ src/PDSCinema/service/ClienteStrategy/ClienteEvMusical.java
@@ -4,8 +4,17 @@ import PDSCinema.model.Evento;
 
 public class ClienteEvMusical extends ClienteStrategyAbstractEvento{
 
+    PDSCinema.DAO.ClienteStrategy.ClienteEvMusical clienteEvMusical = (PDSCinema.DAO.ClienteStrategy.ClienteEvMusical) SingletonEventoDAO.getCliente().getClienteStrategyEventoDAO();
+
     @Override
-    public Evento buscarEvento(String nome) {
-        return null;
+    public Evento buscarEvento(String nome){
+        try {
+            if(!EvMusicalRepository.getShowsEmCartaz().isEmpty()) {
+                return SingletonEventoDAO.getAdm().buscarEvento(nome);
+            }
+            return null;
+        }catch(Exception e){
+            return null;
+        }
     }
 }
