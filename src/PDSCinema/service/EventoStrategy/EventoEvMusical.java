b60cd11196c9992be52313e8576296f0b49f21a0
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-24 15:22:10 -0300

    Update EventoEvMusical.java

--- src/PDSCinema/service/EventoStrategy/EventoEvMusical.java
+++ src/PDSCinema/service/EventoStrategy/EventoEvMusical.java
@@ -7,13 +7,52 @@ import java.util.List;
 
 public class EventoEvMusical extends EventoStrategyAbstractEvento{
 
-    @Override
-    public List<Double> calcularMediaAvaliacaoEvento(List<Evento> eventos) {
-        return null;
+   public ArrayList<String> calcularRanking(List<Evento> listaDeEventos){
+        ArrayList<Show> shows = new ArrayList<>();
+        for(Evento evento : listaDeEventos){
+            shows.add((Show) evento);
+        }
+        List<Double> medias = calcularMediaAvaliacaoEvento(listaDeEventos);
+        ArrayList<String> rankingShows = new ArrayList<>();
+        EventoRepository.setMedias(medias);
+
+        for(Show show : Shows)
+            rankingShows.add(show.getName());
+
+        int i, j, min;
+        Double aux;
+        String aux2;
+        for (i = 0; i < (listaDeEventos.size())-1; i++){
+            min = i;
+            for (j = (i + 1); j < listaDeEventos.size(); j++) {
+                if (medias.get(j) < medias.get(min))
+                    min = j;
+            }
+            if (i != min) {
+                aux = medias.get(i);
+                aux2 = rankingShows.get(i);
+                medias.set(i, medias.get(min));
+                medias.set(min, aux);
+                rankingShows.set(i, rankingShows.get(min));
+                rankingShows.set(min, aux2);
+            }
+        }
+        return rankingShows;
     }
 
-    @Override
-    public ArrayList<String> calcularRanking(List<Evento> listaDeEventos){
-        return null;
+    public List<Double> calcularMediaAvaliacaoEvento(List<Evento> eventos) {
+        try{
+            List<Double> valor = new ArrayList<>();
+            for (Evento evento : eventos) {
+                if(evento.getQuantAvaliacoes() == 0){
+                    valor.add(((double)(evento.getAvaliacoes())));
+                }else{
+                    valor.add(((double)(evento.getAvaliacoes()) / (double)(evento.getQuantAvaliacoes())));
+                }
+            }
+            return valor;
+        }catch (ArithmeticException e){
+            return null;
+        }
     }
 }
