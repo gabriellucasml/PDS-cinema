73dde2134d1d9ac9686d739e406c26614596b7cf
Acsa Arcanjo <44912056+acsa-laa@users.noreply.github.com>
2021-04-24 15:11:10 -0300

    Update AdmEvMusical.java

--- src/PDSCinema/service/AdmStrategy/AdmEvMusical.java
+++ src/PDSCinema/service/AdmStrategy/AdmEvMusical.java
@@ -5,11 +5,21 @@ import PDSCinema.model.Evento;
 public class AdmEvMusical extends AdmStrategyAbstractEvento{
 
     @Override
-    public Evento buscarEvento(String nome){
-        return null;
-    }
-    @Override
     public int removerEvento(Evento evento){
-        return 0;
+        if(EvMusicalRepository.getShowEmCartaz().contains(evento)) {
+            SingletonEventoDAO.getAdm().removerEvento(evento);
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+
+    @Override
+    public Evento buscarEvento(String evento){
+        if(!EvMusicalRepository.getShowEmCartaz().isEmpty()) {
+            return SingletonEventoDAO.getAdm().buscarEvento(evento);
+        }else{
+            return null;
+        }
     }
 }
